58ff2d7c27d00f75ff4ff64ba77b0bd19d6afbe9
Maxim Semikov <spinnen.87@mail.ru>
2019-08-16 03:15:14 +0600

    deployed chat

---
+++ precache-manifest.80de035cbc8f515714b524f4cccbd983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f63c57ba9334aaef2b838635550479",
+    "url": "/ws-chat/index.html"
+  },
+  {
+    "revision": "cdfde05a3a0f93d68ff6",
+    "url": "/ws-chat/static/css/main.5c63ab5b.chunk.css"
+  },
+  {
+    "revision": "e01411c44b269185ff30",
+    "url": "/ws-chat/static/js/2.597a040c.chunk.js"
+  },
+  {
+    "revision": "cdfde05a3a0f93d68ff6",
+    "url": "/ws-chat/static/js/main.9ae5543e.chunk.js"
+  },
+  {
+    "revision": "421eb4374afb89507ca5",
+    "url": "/ws-chat/static/js/runtime~main.74011521.js"
+  }
+]);
